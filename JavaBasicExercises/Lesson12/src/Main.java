9e4d12bb0382dee55acd2a4d7b8bc4bb2d27d86b
Oren Nudelman <orennu@gmail.com>
2020-02-04 23:10:23 +0200

    uncomment previous merges

--- JavaBasicExercises/Lesson12/src/Main.java
+++ JavaBasicExercises/Lesson12/src/Main.java
@@ -7,27 +7,27 @@ public class Main {
 		Matrixes matrixes = new Matrixes();
 
 		// Execute Random Matrix
-//		System.out.println("Random 2x2 Matrix\n===========================");
-//		matrixes.randomMatrix(2);
-//
-//		// Execute Is Matrix Ascending
-//		System.out.println("\nCheck if matrixes ascending\n===========================");
-//		int[][][] matrixMocksArray = matrixes.matrixMocksArray;
-//
-//		for (int matrix = 0; matrix < matrixMocksArray.length; matrix++) {
-//			System.out.println(Arrays.deepToString(matrixMocksArray[matrix]) + ": "
-//					+ matrixes.isMatrixAscending(matrixMocksArray[matrix]));
-//		}
-		
+		System.out.println("Random 2x2 Matrix\n===========================");
+		matrixes.randomMatrix(2);
+
+		// Execute Is Matrix Ascending
+		System.out.println("\nCheck if matrixes ascending\n===========================");
+		int[][][] matrixMocksArray = matrixes.matrixMocksArray;
+
+		for (int matrix = 0; matrix < matrixMocksArray.length; matrix++) {
+			System.out.println(Arrays.deepToString(matrixMocksArray[matrix]) + ": "
+					+ matrixes.isMatrixAscending(matrixMocksArray[matrix]));
+		}
+
 		// Execute find row with sum of given number
 		int[][] checkRowSumMatrix = matrixes.checkRowSumMatrix;
 		String strNum = JOptionPane.showInputDialog("Enter number to check");
 		int num = Integer.parseInt(strNum);
-//		System.out.println("\nCheck if number equals to row sum in matrix\n============================================");
-//		System.out.println("On matrix\n");
-//		matrixes.prettyPrintMatrix(checkRowSumMatrix);
-//		matrixes.checkRowSum(checkRowSumMatrix, num);
-		
+		System.out.println("\nCheck if number equals to row sum in matrix\n============================================");
+		System.out.println("On matrix\n");
+		matrixes.prettyPrintMatrix(checkRowSumMatrix);
+		matrixes.checkRowSum(checkRowSumMatrix, num);
+
 		// Execute find column with sum of given number
 		System.out.println("\nCheck if number equals to column sum in matrix\n============================================");
 		System.out.println("On matrix\n");
