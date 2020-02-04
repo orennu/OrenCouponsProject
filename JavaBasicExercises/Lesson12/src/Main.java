09ed5d9dfbde2b8bf6374874bd698974592395b3
Oren Nudelman <orennu@gmail.com>
2020-02-04 09:35:35 +0200

    pretty printing

--- JavaBasicExercises/Lesson12/src/Main.java
+++ JavaBasicExercises/Lesson12/src/Main.java
@@ -1,17 +1,21 @@
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
 
 		Matrixes matrixes = new Matrixes();
 
-		// Execute Random Matrix 
+		// Execute Random Matrix
+		System.out.println("Random 2x2 Matrix\n===========================");
 		matrixes.randomMatrix(2);
 
 		// Execute Is Matrix Ascending
+		System.out.println("\nCheck if matrixes ascending\n===========================");
 		int[][][] matrixMocksArray = matrixes.matrixMocksArray;
 
 		for (int matrix = 0; matrix < matrixMocksArray.length; matrix++) {
-			System.out.println(matrixes.isMatrixAscending(matrixMocksArray[matrix]));
+			System.out.println(Arrays.deepToString(matrixMocksArray[matrix]) + ": "
+					+ matrixes.isMatrixAscending(matrixMocksArray[matrix]));
 		}
 	}
 }
