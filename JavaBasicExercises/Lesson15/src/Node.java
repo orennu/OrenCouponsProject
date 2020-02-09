a8acd8d0284b81ddc1a08f6e29436236590e6603
Oren Nudelman <orennu@gmail.com>
2020-02-09 21:05:43 +0200

    added exercises from lesson 15

---
+++ JavaBasicExercises/Lesson15/src/Node.java
@@ -0,0 +1,41 @@
+
+public class Node {
+	
+	private int value;
+	private Node next;
+	private Node previous;
+	
+	public Node(int value) {
+		this.value = value;
+		this.next = null;
+	}
+	
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Node previous) {
+		this.previous = previous;
+	}
+}
